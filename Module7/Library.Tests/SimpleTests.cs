26a965d04aa7cc70295485db48448a2623d1c8f9
Shakhzod Abdulloev <abdulloev-2005@mail.ru>
2025-11-12 02:40:21 +0300

    feat(task-787): add simple test with theory

--- Module7/Library.Tests/SimpleTests.cs
+++ Module7/Library.Tests/SimpleTests.cs
@@ -9,4 +9,14 @@ public class SimpleTests
         
         Assert.Equal(4, result);
     }
+    
+    [Theory]
+    [InlineData(1, 2, 1)]
+    [InlineData(5, 5, 3125)]
+    public void Pow_ReturnsExpectedResult(double x, double y, double expected)
+    {
+        var result = Math.Pow(x, y);
+
+        Assert.Equal(expected, result, precision: 10);
+    }
 }
